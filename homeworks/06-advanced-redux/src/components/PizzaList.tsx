b7febbcff78a177946f75cab75972d20299ab257
DmitriyFriz <dmitriy.bulavko.friz44@gmail.com>
2021-05-04 22:37:45 +0300

    feat: add render pizza list

--- homeworks/06-advanced-redux/src/components/PizzaList.tsx
+++ homeworks/06-advanced-redux/src/components/PizzaList.tsx
@@ -5,16 +5,19 @@ import { Pizza } from '../types';
 
 interface PizzaListProps {
   pizza: Pizza[];
-  onAdd: (_id: string) => void;
 }
 
-export function PizzaList({ pizza, onAdd }: PizzaListProps) {
-  return R.map((p) =>
-    <PizzaItem
-      key={p._id}
-      _id={p._id}
-      name={p.name}
-      price={p.price}
-      onAdd={onAdd}
-    />, pizza);
+export function PizzaList({ pizza }: PizzaListProps) {
+  return (
+    <>
+      {R.map((p) =>
+        <PizzaItem
+          key={p._id}
+          _id={p._id}
+          name={p.name}
+          price={p.price}
+          onAdd={() => console.log(p._id)}
+        />, pizza)}
+    </>
+  );
 }
